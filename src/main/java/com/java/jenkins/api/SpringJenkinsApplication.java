2fac28356ab0d2c37948553c7d9a3df4ba3d11c9
unknown <mukeshsharma31994@gmail.com>
2021-10-28 18:17:01 +0100

    controller added

--- src/main/java/com/java/jenkins/api/SpringJenkinsApplication.java
+++ src/main/java/com/java/jenkins/api/SpringJenkinsApplication.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsApplication {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
@@ -20,6 +23,11 @@ public class SpringJenkinsApplication {
 	public static void main(String[] args) {
 		logger.info("Application executed");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to Docker by MUKESH";
 	}
 
 }
